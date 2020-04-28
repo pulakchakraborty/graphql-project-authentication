55159582bba5c676db51222e65d10b6c9a8a3c66
Pulak Chakraborty <pulakchakraborty.contact@gmail.com>
2020-04-28 21:05:09 +0200

    Client setup - wiring up ApolloProvider

--- client/index.js
+++ client/index.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ApolloClient from 'apollo-client';
+import { ApolloProvider } from 'react-apollo';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+
+const cache = new InMemoryCache();
+const link = new HttpLink({
+    uri: 'http://localhost:4000/'
+});
+
+const client = new ApolloClient({
+    cache,
+    link
+});
 
 const Root = () => {
-  return (
-    <div>
-      React Frontend
-    </div>
-  );
+    return (
+        <ApolloProvider client={client}>
+            <div>
+                React Frontend
+            </div>
+        </ApolloProvider>
+    );
 };
 
 ReactDOM.render(<Root />, document.querySelector('#root'));
