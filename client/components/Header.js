75732b2ded10de6853674eea7ea10cab0df1be9b
Pulak Chakraborty <pulakchakraborty.contact@gmail.com>
2020-04-28 23:21:01 +0200

    automatic re-render using refetchQueries

--- client/components/Header.js
+++ client/components/Header.js
@@ -7,7 +7,9 @@ import mutation from '../mutations/Logout';
 
 class Header extends Component {
     onLogoutClick = () => {
-        this.props.mutate({});
+        this.props.mutate({
+            refetchQueries: [{ query }]
+        });
     }
 
     renderButtons = () => {
