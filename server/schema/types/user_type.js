11f698dfcb397bdb1baafd5c40d70548bf681bb5
Pulak Chakraborty <pulakchakraborty.contact@gmail.com>
2020-04-28 20:31:01 +0200

    add an id field in UserType

--- server/schema/types/user_type.js
+++ server/schema/types/user_type.js
@@ -2,12 +2,14 @@ const graphql = require('graphql');
 
 const {
     GraphQLObjectType,
-    GraphQLString
+    GraphQLString,
+    GraphQLID
 } = graphql;
 
 const UserType = new GraphQLObjectType({
     name: 'User',
     fields: {
+        id: { type: GraphQLID },
         email: { type: GraphQLString }
     }
 });
