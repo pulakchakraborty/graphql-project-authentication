e329218657a76a0b58d98608e1ec05519577a9d9
Pulak Chakraborty <pulakchakraborty.contact@gmail.com>
2020-04-28 22:50:19 +0200

    render different buttons in header component depending on auth state

--- client/components/Header.js
+++ client/components/Header.js
@@ -4,12 +4,32 @@ import { graphql } from 'react-apollo';
 import query from '../queries/CurrentUser';
 
 class Header extends Component {
+    renderButtons = () => {
+        const { loading, user } = this.props.data;
+        // While data is still being fetched from the endpoint
+        if (loading) {
+            return <div />;
+        }
+
+        // Condition to check if user is logged in or not
+        if (user) {
+            return(
+                <div>Logout</div>
+            );
+        } else {
+            return(
+                <div>User not logged in</div>
+            );
+        }
+
+    }
+
     render() {
         console.log(this.props);
         return(
-            <div>
-                Header Component
-            </div>
+            <nav>
+                <div className="nav-wrapper">{this.renderButtons()}</div>
+            </nav>
         );
     }
 }
