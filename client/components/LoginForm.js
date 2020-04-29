282dfc2b207da88f049fc41263f2d5dbcdb6e7a1
Pulak Chakraborty <pulakchakraborty.contact@gmail.com>
2020-04-29 23:58:37 +0200

    create a AuthForm component and include a form

--- client/components/LoginForm.js
+++ client/components/LoginForm.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import AuthForm from './AuthForm';
 
 class LoginForm extends Component {
     render() {
         return(
-            <div>LoginForm Component</div>
+            <div>
+                <h4>Login</h4>
+                <AuthForm />
+            </div>
         );
     }
 };
