72741dc902c5d5b01c8a11f41f08288d587763cb
Pulak Chakraborty <pulakchakraborty.contact@gmail.com>
2020-04-29 23:38:07 +0200

    Add LoginForm component - create LoginForm component; TODO: include a Form - create a route /login

---
+++ client/components/LoginForm.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class LoginForm extends Component {
+    render() {
+        return(
+            <div>LoginForm Component</div>
+        );
+    }
+};
+
+export default LoginForm;
