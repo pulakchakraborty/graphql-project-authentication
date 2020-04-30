6fb702d1cb91a0691ae64bdab23baa620720276c
Pulak Chakraborty <pulakchakraborty.contact@gmail.com>
2020-04-30 11:34:14 +0200

    create a signup route and component - TODO: import AuthForm in SignupForm and include the mutation

---
+++ client/components/SignupForm.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SignupForm extends Component {
+    render() {
+        return(
+            <div>
+                <h4>Signup</h4>
+            </div>
+        );
+    }
+}
+
+export default SignupForm;
