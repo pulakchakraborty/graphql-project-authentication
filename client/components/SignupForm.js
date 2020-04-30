d32bb9ae49e4ea22dee817e627cd0d28f833ad2f
Pulak Chakraborty <pulakchakraborty.contact@gmail.com>
2020-04-30 12:09:08 +0200

    refetch current user to update the header

--- client/components/SignupForm.js
+++ client/components/SignupForm.js
@@ -3,6 +3,7 @@ import { graphql } from 'react-apollo';
 
 import AuthForm from './AuthForm';
 import mutation from '../mutations/Signup';
+import query from '../queries/CurrentUser';
 
 class SignupForm extends Component {
     constructor(props) {
@@ -15,7 +16,8 @@ class SignupForm extends Component {
             variables: {
                 email,
                 password
-            }
+            },
+            refetchQueries: [{ query }]
         }).catch(res => {
             const errors = res.graphQLErrors.map(error => error.message);
             this.setState({ errors })
