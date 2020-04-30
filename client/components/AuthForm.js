539c69fb8a0b82363d7e4c518eb14db7aa2209fd
Pulak Chakraborty <pulakchakraborty.contact@gmail.com>
2020-04-30 02:30:24 +0200

    Fix the styling in AuthForm

--- client/components/AuthForm.js
+++ client/components/AuthForm.js
@@ -17,15 +17,16 @@ class AuthForm extends Component {
             <div className="row">
                 <form className="col s4">
                     <div className="input-field">
-                        <label>Email</label>
                         <input
+                            placeholder="Email"
                             value={this.state.email}
                             onChange={e => this.setState({ email: e.target.value })}
                         />
                     </div>
                     <div className="input-field">
-                        <label>Password</label>
                         <input
+                            placeholder="Password"
+                            type="password"
                             value={this.state.password}
                             onChange={e => this.setState({ password: e.target.value })}
                         />
